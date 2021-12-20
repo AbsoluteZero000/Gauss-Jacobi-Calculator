26868c53264bc7d596b621fcee0cc9f38e429d70
Ahmed Wael <ahmedwaelwanas@gmail.com>
2021-12-20 07:06:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+
+function checkerror(){
+ 
+    let X=document.getElementById("Solution").value;
+    console.log(X);
+
+
+
+
+
+}
+function arrangerows(){
+
+
+
+
+}
+function calculate(){
+
+
+
+}
+
+
+
+function Clear(){
+
+
+    document.getElementById("matrix").value=null;
+    document.getElementById("Solution").value=null;
+    document.getElementById("iteration").value=1;
+
+}
