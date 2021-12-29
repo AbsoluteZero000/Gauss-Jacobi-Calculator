c1a3ed0b271ea1286a4196d77ed029a890d232a9
Ahmed Wael <87247990+AbsoluteZero000@users.noreply.github.com>
2021-12-29 20:56:12 +0200

    it's finally works

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function printOnPage(text) {
 function arrangerows() {
   for (let j = 0; j < matrix.length; j++) {
     for (let i = 0; i < matrix.length; i++) {
-      if (matrix[i][i] < matrix[j][i]) {
+      if (Math.abs(matrix[i][i]) < Math.abs(matrix[j][i])) {
         //Swap co-efficient row by pointers
         co_ef_swapper = matrix[i];
         matrix[i] = matrix[j];
